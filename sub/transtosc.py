3a65e3ff3a9ee3cba73095ed9a1382d9fec0ca94
AO2233 <59056753+AO2233@users.noreply.github.com>
2022-02-22 19:05:57 +0800

    Update transtosc.py

--- sub/transtosc.py
+++ sub/transtosc.py
@@ -1,55 +1,52 @@
-import opencc
+import argparse
 import os
-import sys
+import opencc
 from tqdm import tqdm
 
-helpstr = ('\n'
-           'help\n'
-           '本python脚本用来处理字幕繁简转换问题\n'
-           '使用示例：\n'
-           'python3 transtosc.py \'放有字幕的path_1\' \'放有字幕的path_2\'\n'
-           '本脚本只读取 srt ass ssa 后缀文件\n'
-           '转换完毕的文件会放置在下级目录的new之中\n'
-           'PS:\n'
-           '本脚本只读取一层目录 AND 只支持 UTF-8,GBK等编码\n')
-
-
-def t2s(i):
+def t2s(i, use_new=False, mode='t2s'):
     ass_files = [name for name in os.listdir(i)
                  if (name.endswith('.ass') or name.endswith('.srt') or name.endswith('.ssa'))]
     bar = tqdm(range(len(ass_files)))
     for data in ass_files:
         old_data = i + '/' + data
-        new = (i + '/new')
-        isExists = os.path.exists(new)
-        if not isExists:
-            os.mkdir(new)
-
+        if use_new:
+            new = (i + '/new')
+            isExists = os.path.exists(new)
+            if not isExists:
+                os.mkdir(new)
+        else:
+            new = i
         try:
-            f = open(old_data, 'rt',encoding='utf8')
-            unicode='utf8'
+            f = open(old_data, 'rt', encoding='utf8')
+            unicode = 'utf8'
             reader = f.read()
         except UnicodeDecodeError:
             try:
                 f = open(old_data, 'rt', encoding='gbk')
-                unicode='gbk'
+                unicode = 'gbk'
                 reader = f.read()
             except UnicodeDecodeError:
                 f = open(old_data, 'rt', encoding='utf16')
-                unicode='utf16'
+                unicode = 'utf16'
                 reader = f.read()
-        tem = opencc.OpenCC('t2s')
+        tem = opencc.OpenCC(mode)
         new_data = new + '/' + data
-        nf = open(new_data, 'wt',encoding=unicode)
+        nf = open(new_data, 'wt', encoding=unicode)
         nf.write(tem.convert(reader))
         bar.update(1)
         bar.set_description("正在处理 %s" % data)
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "-h" or sys.argv[1] == "--help" :
-        print(helpstr)
-        exit()
-
-    for i in tqdm(sys.argv[1:]):
-            t2s(i)
+    parse = argparse.ArgumentParser("opencc 繁体转简体 简体转繁体", epilog='支持UTF-8,UTF-16,GBK编码')
+    parse.add_argument('-p', nargs='*', default='', help="文件路径")
+    parse.add_argument('-R', default=False,help="是否替换 True or False")
+    parse.add_argument('-m', default='t2s', help="'t2s 繁转简 s2t 简转繁'")
+    parse.format_help()
+    arg = parse.parse_args()
+    dic_all = []
+    for i in arg.p:
+        for root, sub, file in os.walk(i):
+            dic_all.append(root)
+    for i in dic_all:
+        t2s(i, arg.R, arg.m)
